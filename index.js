3fc2842702ee15e52db5bfc263f10e7f7dc9db25
Biswaranjan Rout <biswaranjanrout.pro@gmail.com>
2026-02-06 03:23:00 +0530

    Initial project setup with Express, MongoDB and routes

---
+++ index.js
@@ -0,0 +1,110 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+
+
+const app = express();
+
+
+
+// Database Connection
+async function databaseConnection() {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/HomeTutorHub");
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.log("MongoDB Connection Failed");
+    console.log(error);
+  }
+}
+
+databaseConnection();
+
+
+// Schema 
+const userSchema = mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        min: [8, "Password must be at least 8 characters"],
+        required: true,
+    }
+}, 
+{timestamps: true}
+);
+
+// Model
+
+const User = mongoose.model("user", userSchema);
+
+// Setting up ejs 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+
+// Global Middlerwares
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+
+// Routes
+
+// register
+app.get("/users/register", (req, res) => {
+    return res.render("register");
+});
+
+app.post("/users/register", async (req, res) => {
+    const {fullName, email, password} = req.body;
+
+    if(!fullName || !email || !password) {
+        return res.status(400).send("all fields are required");
+    }
+
+    await User.create({
+        fullName,
+        email,
+        password
+    });
+
+    return res.status(201).redirect("/");
+});
+
+
+// Login 
+app.get("/users/login", (req, res) => {
+    return res.render("login");
+});
+
+app.post("/users/login",  async (req, res) => {
+    const {email, password} = req.body;
+    const user = await User.findOne({email});
+
+    if(!user) return res.json({msg: "no user found with given email"});
+
+    if(user.password !== password) {
+        return res.json({msg: "wrong password"});
+    }
+    
+    return res.send("You are login");
+});
+
+
+
+
+// Test Route
+app.get("/", (req, res) => {
+    return res.render("home");
+});
+
+
+app.listen(8080, () => console.log("Sever started at port 8080"));
---
+++ readme.md
@@ -0,0 +1,6 @@
+# HomeTutorHub 🏠📚
+
+HomeTutorHub is a backend-focused web application built to understand how real-world authentication systems work using **Node.js, Express, MongoDB, and EJS**.  
+This project is part of my learning journey where I’m strengthening my fundamentals by building features step by step.
+
+---
